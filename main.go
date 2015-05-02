0ef74cac08216a2ce726993a4732f6350ec64bd3
Dimitri Sokolyuk <demon@dim13.org>
2015-05-03 00:08:51 +0200

    Add misc patterns

--- main.go
+++ main.go
@@ -4,41 +4,17 @@ func main() {
 	dev := NewDevice()
 	defer dev.Close()
 
-	cu := NewCutter(dev.Handle(), Portrait)
+	c := NewCutter(dev.Handle(), Portrait)
 
-	defer cu.Home()
-	//defer cu.LineType(Solid)
-	//cu.Raw([]string{"L100,1,400,100"})
+	defer c.Home()
+	//defer c.LineType(Solid)
+	//c.Raw([]string{"L100,1,400,100"})
 
-	//cu.TestCut()
-	//cu.TestPattern()
-	//cu.EasterEgg()
-	//cu.DrawMarks()
-	//cu.Move(Point{300, 300})
-	//cu.SearchMarks()
-	//cu.Bezier(1, Point{0,0},Point{0,1000},Point{0,0},Point{1000,0})
-
-	/*
-		for i := 1; i < 10; i++ {
-			cu.Circle(Point{1000, 1000},
-				Polar{100 * i, 0},
-				Polar{100 * i, 3600})
-		}
-	*/
-
-	//cu.Move(Point{500,500})
-
-	cu.Circle(Point{2000, 2000}, Polar{100, 0}, Polar{100, 3600})
-	for i := 0; i < 3; i++ {
-		cu.Ellipse(0, Point{2000, 2000},
-			Polar{500, 0}, Polar{200, 3600}, 600*i)
-	}
-
-	/*
-		for i := 0; i < 9; i++ {
-			cu.LineType(LineStyle(i))
-			cu.Move(Point{100 * i, 0})
-			cu.Draw(Point{100 * i, 1000})
-		}
-	*/
+	//c.TestCut()
+	//c.TestPattern()
+	//c.EasterEgg()
+	//c.DrawMarks()
+	//c.Move(Point{300, 300})
+	//c.SearchMarks()
+	//c.Bezier(1, Point{0,0},Point{0,1000},Point{0,0},Point{1000,0})
 }
---
+++ misc.go
@@ -0,0 +1,25 @@
+package main
+
+func (c Cutter) DrawAtom() {
+	for i := 0; i < 3; i++ {
+		c.Ellipse(0, Point{2000, 2000},
+			Polar{500, 0}, Polar{200, 3600}, 600*i)
+	}
+	c.Circle(Point{2000, 2000}, Polar{100, 0}, Polar{100, 3600})
+}
+
+func (c Cutter) DrawLines() {
+	for i := 0; i < 9; i++ {
+		c.LineType(LineStyle(i))
+		c.Move(Point{100 * i, 0})
+		c.Draw(Point{100 * i, 1000})
+	}
+}
+
+func (c Cutter) DrawCircles() {
+	for i := 1; i < 10; i++ {
+		cu.Circle(Point{1000, 1000},
+			Polar{100 * i, 0},
+			Polar{100 * i, 3600})
+	}
+}
