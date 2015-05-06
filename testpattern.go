23d784959cce41ece1ef0f1ee3c143a54d72f029
Dimitri Sokolyuk <demon@dim13.org>
2015-05-06 21:31:31 +0200

    whitespaces

--- testpattern.go
+++ testpattern.go
@@ -14,14 +14,18 @@ func (c Cutter) TestPattern() {
 		Point{637, 439}, Point{637, 510})
 	c.Bezier(1, Point{637, 510}, Point{637, 580},
 		Point{580, 637}, Point{510, 637})
+
 	c.Move(Point{764, 764})
 	c.Draw(Point{256, 764})
 	c.Draw(Point{256, 256})
 	c.Draw(Point{764, 256})
 	c.Draw(Point{764, 764})
+
 	c.Move(Point{2, 510})
 	c.Draw(Point{1018, 510})
+
 	c.Move(Point{510, 1018})
 	c.Draw(Point{510, 2})
+
 	c.Move(Origin)
 }
