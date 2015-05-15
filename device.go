bc49062cdd593fca1a40116b31d41823c11a8a32
Dimitri Sokolyuk <demon@dim13.org>
2015-05-15 19:36:42 +0200

    Move NewDevice out of main

--- device.go
+++ device.go
@@ -1,8 +1,28 @@
 package main
 
-import "bufio"
+import (
+	"bufio"
+	"log"
+	"runtime"
+)
 
 type Device interface {
 	Close()
 	Handle() *bufio.ReadWriter
+}
+
+func NewDevice() (dev Device) {
+	var err error
+
+	if runtime.GOOS == "linux" {
+		dev, err = NewLP("/dev/usb/lp0")
+	} else {
+		dev, err = NewUSB()
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
 }
--- main.go
+++ main.go
@@ -1,52 +1,11 @@
 package main
 
-import (
-	"flag"
-	"log"
-	"runtime"
-)
-
-var (
-	dev Device
-	err error
-	cmd = flag.String("cmd", "", "command")
-)
-
 func main() {
-	flag.Parse()
-
-	if runtime.GOOS == "linux" {
-		dev, err = NewLP("/dev/usb/lp0")
-	} else {
-		dev, err = NewUSB()
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	dev := NewDevice()
 	defer dev.Close()
 
 	handle := dev.Handle()
-
-	//c := NewCutter(handle, Portrait, 0)
-
 	defer Home(handle.Writer)
-	//defer c.LineType(Solid)
-	//c.Raw([]string{"L100,1,400,100"})
-
-	//c.TestCut()
-	//c.TestPattern()
-	//c.EasterEgg()
-	//c.DrawMarks(Point{}, Point{}, 0)
-	//c.WriteUpperRight(Point{4000,5440})
-	//fmt.Println(c.UnknownFQ5())
-	//c.Bezier(1, Point{0,0},Point{0,1000},Point{0,0},Point{1000,0})
-
-	//fmt.Println("Gin", c.Gin())
-	//fmt.Println("Call Gin", c.CallGin())
-
-	//c.MustMarks(Point{18 * CM, 19 * CM}, Type2)
 
 	Initialize(handle, 113, Portrait)
 	A4.UpperRight(handle.Writer)
