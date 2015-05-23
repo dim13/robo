62e48700647f88768296ec193a0409785538e9b9
Dimitri Sokolyuk <demon@dim13.org>
2015-05-23 20:12:18 +0200

    Revert

--- device_openbsd.go
+++ device_openbsd.go
@@ -1,12 +1,10 @@
 // note: ulpt* doesn't support read(), thus this dev is broken atm.
-// use instead ugen* by disabling ulpt at boot time
-// see config(8) for details
 package robo
 
 import "log"
 
 func NewDevice() Device {
-	dev, err := NewLP("/dev/ugen0.00")
+	dev, err := NewLP("/dev/ulpt0")
 	if err != nil {
 		log.Fatal(err)
 	}
