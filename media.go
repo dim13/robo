58d8dd7e74dd8b4a8129a643b6c2d48811a84c5f
Dimitri Sokolyuk <demon@dim13.org>
2018-01-02 03:41:59 +0100

    gofmt

--- media.go
+++ media.go
@@ -36,63 +36,63 @@ func (m Media) Apply(c *bufio.Writer) {
 }
 
 var Medias = []Media{
-	Media{
+	{
 		Descr:   "Card without Craft Paper Backing",
 		ID:      100,
 		Speed:   10,
 		Force:   27,
 		Overcut: 18,
 	},
-	Media{
+	{
 		Descr:   "Card with Craft Paper Backing",
 		ID:      101,
 		Speed:   10,
 		Force:   27,
 		Overcut: 18,
 	},
-	Media{
+	{
 		Descr:   "Vinyl Sticker",
 		ID:      102,
 		Speed:   10,
 		Force:   10,
 		Overcut: 18,
 	},
-	Media{
+	{
 		Descr:   "Film Labels",
 		ID:      106,
 		Speed:   10,
 		Force:   14,
 		Overcut: 18,
 	},
-	Media{
+	{
 		Descr:   "Magnetic Sheet",
 		ID:      107,
 		Speed:   10,
 		Force:   12,
 		Overcut: 18,
 	},
-	Media{
+	{
 		Descr:   "Thick Media",
 		ID:      111,
 		Speed:   10,
 		Force:   27,
 		Overcut: 18,
 	},
-	Media{
+	{
 		Descr:   "Thin Media",
 		ID:      112,
 		Speed:   10,
 		Force:   2,
 		Overcut: 18,
 	},
-	Media{
+	{
 		Descr:   "Pen",
 		ID:      113,
 		Speed:   10,
 		Force:   10,
 		Overcut: 0,
 	},
-	Media{
+	{
 		Descr:   "Custom",
 		ID:      300,
 		Speed:   10,
