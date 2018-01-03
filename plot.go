cd0ec6a1dfc1134f8b7af7592ae12c0e8a14258a
Dimitri Sokolyuk <demon@dim13.org>
2018-01-03 01:30:39 +0100

    ...

--- plot.go
+++ plot.go
@@ -26,37 +26,6 @@ type Plotter interface {
 	Plot() []byte
 }
 
-type Line []Point
-type Bezier [4]Point
-type Circle [3]Point
-
-func (v Line) Plot() []byte   { return nil } // MDDDD...
-func (v Bezier) Plot() []byte { return nil } // BZ...
-func (v Circle) Plot() []byte { return nil } // W...
-
-func join(p ...Point) string {
-	path := make([]string, len(p))
-	for i, v := range p {
-		path[i] = v.String()
-	}
-	return strings.Join(path, ",")
-}
-
-// ???
-func Draw(p ...Point) string          { return fmt.Sprintf("D%v", Path(p)) }
-func DrawRelateive(p ...Point) string { return fmt.Sprintf("E%v", Path(p)) }
-func Move(p Point) string             { return fmt.Sprintf("M%v", p) }
-func MoveRelative(p Point) string     { return fmt.Sprintf("O%v", p) }
-
-//func Offset(p Point) string          { return "^" + p.String() }
-//func LowerLeft(p Point) string   { return "\\" + p.String() }
-//func UpperRight(p Point) string  { return "Z" + p.String() }
-func CuttingArea(p Point) string { return "FU" + p.String() }
-
-//func Calibration(p Point) string     { return "TB72" + p.String() }
-
-func Curve(a int, p ...Point) string { return fmt.Sprintf("Y%d,%v", a, Path(p)) }
-
 func (r Robo) Wait4Ready() {
 	t := time.NewTicker(time.Second)
 	defer t.Stop()
@@ -96,6 +65,7 @@ func (r Robo) GoHome()                         { r.Printf("TT") }
 func (r Robo) Home()                           { r.Printf("H") }
 func (r Robo) Origin()                         { r.Printf("FJ") }
 func (r Robo) Calibrate()                      { r.Printf("TB70") }
+func (r Robo) Calibration(p Point)             { r.Printf("TB72,%v", p) }
 func (r Robo) TestCut()                        { r.Printf("FH") }
 func (r Robo) TestLoop()                       { r.Printf("FI") }
 func (r Robo) Factor(x, y, z Unit)             { r.Printf("&%v", Triple{x, y, z}) }
@@ -128,6 +98,7 @@ func (r Robo) TrackEnhancing(u Unit)     { r.Printf("FY%v", u) }
 func (r Robo) RegMarkLen(u Unit)         { r.Printf("TB51,%v", u) }
 
 func (r Robo) Orientation(o Orientation) { r.Printf("FN%d", o) }
+func (r Robo) Curve(a int, p ...Point)   { r.Printf("Y%d,%v", a, Path(p)) }
 
 func (r Robo) Initialize(m Media, o Orientation) {
 	r.Init()
