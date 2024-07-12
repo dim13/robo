561f479d350432b62ef168e5d066e94d7875b77c
Dimitri Sokolyuk <sokolyuk@gmail.com>
2024-07-12 21:49:31 +0200

    Copy out replies

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"log"
 	"os"
 
@@ -14,6 +15,13 @@ func main() {
 		log.Fatal(err)
 	}
 	defer dev.Close()
+
+	robo.Initialize(dev, 113, robo.Portrait)
+	robo.A4.Sub(robo.Margin).UpperRight(dev)
+	robo.Triple{U: 100, V: 100, W: 100}.Factor(dev)
+
+	go io.Copy(os.Stdout, dev)
+
 	s := bufio.NewScanner(os.Stdin)
 	for s.Scan() {
 		robo.Send(dev, s.Text())
